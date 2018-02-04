af22e57bba3ee79458f8898596a34e2ade4b0606
tayurus <tayurus@gmail.com>
2018-02-04 09:40:51 +0300

    update project to new template

--- src/gulpFile.js
+++ src/gulpFile.js
@@ -1,83 +1,49 @@
-var gulp = require('gulp');
-var rename = require('gulp-rename');
+let gulp = require("gulp");
+let jade = require("gulp-jade");
+let autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
-var urlAdjuster = require('gulp-css-url-adjuster');
-var jade = require('gulp-jade');
+var rename = require('gulp-rename');
 
-var srcPath = {
-    jadePagesSrc: ["jade/**/*.jade"],
-    jadeBlocks: ["blocks/**/*.jade"],
-    htmlSrc: "html/**/*.html",
-    imgSrc: ["**/*.png", "**/*.jpg", "**/*.svg"],
-    cssSrc: ["**/*.css", "!node_modules/**/*.css"],
-    jsSrc: [  "**/*.js", "!gulpFile.js", "!node_modules/**/*.js"]
+// SOURCES
+let src = {
+    css: ["**/*.css", "!node_modules/**/*.css"],
+    js: ["**/*.js", "!node_modules/**/*.js", "!gulpFile.js"],
+    pages: ["pages/**/*.jade", "blocks/**/*.jade"],
+    img: ["**/*.png", "**/*.jpg", "**/*.svg"],
 }
 
-var distSrc = {
-    dist: "../dist/",
-    distImg: "../dist/img/",
+let dist = {
+    main: "../dist/",
+    img: "../dist/img"
 }
+ 
+gulp.task("build", function() {
+    //PAGES
+    gulp.src(src.pages).pipe(jade({
+        pretty: "\t"
+    })).pipe(gulp.dest(dist.main));
 
-gulp.task('buildForLeha', function() {
-
-    gulp.src(srcPath.jadePagesSrc)
-   .pipe(jade({
-       pretty : "\t"
-   }))
-   .pipe(gulp.dest(distSrc.dist));
-
-   gulp.src(srcPath.htmlSrc).pipe(gulp.dest(distSrc.dist));
-
-
-    gulp.src(srcPath.htmlSrc).pipe(gulp.dest(distSrc.dist));
-
-    gulp.src(srcPath.cssSrc).pipe(rename({
+    // IMG
+    gulp.src(src.img).pipe(rename({
         dirname: ''
-    })).pipe(concat("styles.css")).pipe(urlAdjuster({
-        replace: ['img/', '../img/NewTemplate/img/']
-    })).pipe(urlAdjuster({
-        replace: ['fonts/', '../fonts/']
-    }))
-    .pipe(gulp.dest(distSrc.dist));
+    })).pipe(gulp.dest(dist.img));
 
+    //CSS
+    let autoprefixBrowsers = ['> 1%', 'last 2 versions', 'firefox >= 4', 'safari 7', 'safari 8', 'IE 8', 'IE 9', 'IE 10', 'IE 11'];
+    gulp.src(src.css)
+        .pipe(concat("styles.css"))
+        .pipe(autoprefixer({
+            browsers: autoprefixBrowsers
+        }))
+        .pipe(gulp.dest(dist.main));
 
+    //JS
+    gulp.src(src.js).pipe(concat("script.js")).pipe(gulp.dest(dist.main));
 
-    gulp.src(srcPath.jsSrc).pipe(rename({
-        dirname: ''
-    })).pipe(concat("script.js")).pipe(gulp.dest(distSrc.dist));
+    //FONTS
+    gulp.src("fonts/**/*.*",  { base: './' }).pipe(gulp.dest(dist.main));
+})
 
-
-    gulp.src("fonts/**/*.*",  { base: './' }).pipe(gulp.dest("../dist/"));
-    gulp.src(srcPath.imgSrc).pipe(rename({
-        dirname: ''
-    })).pipe(gulp.dest(distSrc.distImg));
-});
-
-gulp.task('build', function() {
-
-     gulp.src(srcPath.jadePagesSrc)
-    .pipe(jade({
-        pretty : "\t"
-    }))
-    .pipe(gulp.dest(distSrc.dist));
-
-    gulp.src(srcPath.htmlSrc).pipe(gulp.dest(distSrc.dist));
-
-    gulp.src(srcPath.cssSrc).pipe(rename({
-        dirname: ''
-    })).pipe(concat("styles.css")).pipe(gulp.dest(distSrc.dist));
-
-    gulp.src(srcPath.jsSrc).pipe(rename({
-        dirname: ''
-    })).pipe(concat("script.js")).pipe(gulp.dest(distSrc.dist));
-
-
-    gulp.src("fonts/**/*.*",  { base: './' }).pipe(gulp.dest("../dist/"));
-    gulp.src(srcPath.imgSrc).pipe(rename({
-        dirname: ''
-    })).pipe(gulp.dest(distSrc.distImg));
-});
-
-gulp.task('watch', function() {
-    gulp.watch(Object.values(srcPath), ["build"]);
-});
+gulp.task("watch", ["build"], function() {
+    gulp.watch(Object.values(src), ["build"]);
+})
