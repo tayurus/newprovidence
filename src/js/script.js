5fa51d612886b9b7044f603111bdc2d366987f3f
tayurus <tayurus@gmail.com>
2018-02-07 11:26:41 +0300

    add first slider (not feedback)

---
+++ src/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $(".slick-carousel").slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 5,
+        responsive: [{
+                breakpoint: 800,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '15px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '15px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+    $(".slick-slide").click(function(){
+        $(".slick-carousel").slick('slickGoTo', $(this).attr("data-slick-index"),true);
+    })
+
+});
